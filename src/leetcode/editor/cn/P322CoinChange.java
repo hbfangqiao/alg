51c9492f6e8934a07db937f122f4183a19a972a0
fq <hbfangqiao@163.com>
2021-01-20 01:00:53 +0800

    P322 零钱兑换 练习

---
+++ src/leetcode/editor/cn/P322CoinChange.java
@@ -0,0 +1,102 @@
+//给定不同面额的硬币 coins 和一个总金额 amount。编写一个函数来计算可以凑成总金额所需的最少的硬币个数。如果没有任何一种硬币组合能组成总金额，返回
+// -1。 
+//
+// 你可以认为每种硬币的数量是无限的。 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：coins = [1, 2, 5], amount = 11
+//输出：3 
+//解释：11 = 5 + 5 + 1 
+//
+// 示例 2： 
+//
+// 
+//输入：coins = [2], amount = 3
+//输出：-1 
+//
+// 示例 3： 
+//
+// 
+//输入：coins = [1], amount = 0
+//输出：0
+// 
+//
+// 示例 4： 
+//
+// 
+//输入：coins = [1], amount = 1
+//输出：1
+// 
+//
+// 示例 5： 
+//
+// 
+//输入：coins = [1], amount = 2
+//输出：2
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 1 <= coins.length <= 12 
+// 1 <= coins[i] <= 231 - 1 
+// 0 <= amount <= 104 
+// 
+// Related Topics 动态规划 
+// 👍 1027 👎 0
+
+
+package leetcode.editor.cn;
+
+import java.util.Arrays;
+
+//Java：零钱兑换
+public class P322CoinChange{
+    public static void main(String[] args) {
+        Solution solution = new P322CoinChange().new Solution();
+        // TO TEST
+        int[] coins = {2};
+        System.out.println(solution.coinChange(coins,3));
+    }
+    //leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public int coinChange(int[] coins, int amount) {
+        int[] dp = new int[amount+1];
+        Arrays.fill(dp,amount+1);
+        dp[0] = 0;
+        for (int i = 1; i <=amount ; i++) {
+            for (int j = 0; j <coins.length ; j++) {
+                if (coins[j] <= i){
+                    dp[i] = Math.min(dp[i],dp[i - coins[j]] + 1);
+                }
+            }
+        }
+        return dp[amount] == amount+1 ? -1 : dp[amount];
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+/*
+第一遍：
+1. 5-15分钟读题思考题目 √ 没有dp思路
+官方题解： https://leetcode-cn.com/problems/coin-change/solution/322-ling-qian-dui-huan-by-leetcode-solution/
+
+2. 没有思路则直接看解法，比较解法优劣
+3. 背诵和默写解法
+第二遍
+1. 马上自己写 -> LeeCode提交
+2. 多种解法比较，体会 -> 优化
+第三遍
+1. 过了一天后重复练习相同题目
+第四遍
+1. 过了一周重复练习
+第五遍
+1. 面试前一周重复练习	
+*/
+}
