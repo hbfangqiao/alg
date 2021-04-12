5dd26b4094c26ac061a06e22d0b4245080f895f6
fq <hbfangqiao@163.com>
2021-04-12 10:38:34 +0800

    P151 翻转字符串里的单词 练习

---
+++ src/leetcode/editor/cn/P151ReverseWordsInAString.java
@@ -0,0 +1,111 @@
+//给定一个字符串，逐个翻转字符串中的每个单词。 
+//
+// 说明： 
+//
+// 
+// 无空格字符构成一个 单词 。 
+// 输入字符串可以在前面或者后面包含多余的空格，但是反转后的字符不能包括。 
+// 如果两个单词间有多余的空格，将反转后单词间的空格减少到只含一个。 
+// 
+//
+// 
+//
+// 示例 1： 
+//
+// 输入："the sky is blue"
+//输出："blue is sky the"
+// 
+//
+// 示例 2： 
+//
+// 输入："  hello world!  "
+//输出："world! hello"
+//解释：输入字符串可以在前面或者后面包含多余的空格，但是反转后的字符不能包括。
+// 
+//
+// 示例 3： 
+//
+// 输入："a good   example"
+//输出："example good a"
+//解释：如果两个单词间有多余的空格，将反转后单词间的空格减少到只含一个。
+// 
+//
+// 示例 4： 
+//
+// 输入：s = "  Bob    Loves  Alice   "
+//输出："Alice Loves Bob"
+// 
+//
+// 示例 5： 
+//
+// 输入：s = "Alice does not even like bob"
+//输出："bob like even not does Alice"
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 1 <= s.length <= 104 
+// s 包含英文大小写字母、数字和空格 ' ' 
+// s 中 至少存在一个 单词 
+// 
+//
+// 
+// 
+//
+// 
+//
+// 进阶： 
+//
+// 
+// 请尝试使用 O(1) 额外空间复杂度的原地解法。 
+// 
+// Related Topics 字符串 
+// 👍 308 👎 0
+
+
+package leetcode.editor.cn;
+//Java：翻转字符串里的单词
+public class P151ReverseWordsInAString{
+    public static void main(String[] args) {
+        Solution solution = new P151ReverseWordsInAString().new Solution();
+        // TO TEST
+        System.out.println(solution.reverseWords("  the sky   is blue  "));
+        System.out.println(solution.reverseWords("  hello world!  "));
+        System.out.println(solution.reverseWords("a good   example"));
+        System.out.println(solution.reverseWords("  Bob    Loves  Alice   "));
+    }
+    //leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public String reverseWords(String s) {
+        String[] ss = s.split(" ");
+        StringBuffer result = new StringBuffer();
+        for (int i = ss.length - 1; i >= 0 ; i--) {
+            if (ss[i].equals("")){
+                continue;
+            }
+            result.append(ss[i]).append(" ");
+        }
+        return result.substring(0,result.length() - 1);
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+/*
+第一遍：
+1. 5-15分钟读题思考题目
+2. 没有思路则直接看解法，比较解法优劣
+3. 背诵和默写解法
+第二遍
+1. 马上自己写 -> LeeCode提交
+2. 多种解法比较，体会 -> 优化
+第三遍
+1. 过了一天后重复练习相同题目
+第四遍
+1. 过了一周重复练习
+第五遍
+1. 面试前一周重复练习	
+*/
+}
